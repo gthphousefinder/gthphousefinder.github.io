0098c21c16c9b5c353396378ed0806cd111aace8
Anton Gudiswitz <agudiswitz@gatech.edu>
2020-11-11 00:16:47 -0500

    Made it functional with the full Tax_Parcel data.

--- searchFile.js
+++ searchFile.js
@@ -2,46 +2,38 @@ const fs = require('fs');
 
 //@param minimum total price.
 //@param maximum total price.
-//@param array of zipCodes to include. Default value is all 57 Fulton County GA zipcodes
-function search(totalPriceMin=0, totalPriceMax=10000000, zipCodes=["31106", "31193", "30213", "30268", "30272", "30291", "30296", "30301", "30303", "30305", "30304", "30307", "30306", "30309", "30308", "30311", "30310", "30313", "30312", "30315", "30314", "30316", "30319", "30318", "30321", "30324", "30326", "30325", "30328", "30004", "30327", "30005", "30332", "30331", "30334", "30009", "30337", "30336", "30339", "30342", "30344", "30022", "30349", "30350", "30354", "30358", "30363", "30375", "30384", "30380", "30388", "30398", "30396", "30076", "30075", "30097", "30098"],
-					imprvMax=10000000) {
-  
+//@param array of zipCode to include. Default value is all 57 Fulton County GA zipCode
+function search(jsonZip, totalPriceMin=0, totalPriceMax=10000000, zipCode, imprvMax=10000000) {
+	//If there isn't a zip defined, then throw an error.
+	if (jsonZip == undefined) {
+		throw new ValidationError("Must input zipcode.");
+	}
+
 	//If no input is passed in, fill in values
 	if (totalPriceMin == "" || totalPriceMin == null) {totalPriceMin = 0;}
 	if (totalPriceMax == "" || totalPriceMax == null) {totalPriceMax = 10000000;}
-	if (zipCodes == "" || zipCodes == null) {zipCodes = ["31106", "31193", "30213", "30268", "30272", "30291", "30296", "30301", "30303", "30305", "30304", "30307", "30306", "30309", "30308", "30311", "30310", "30313", "30312", "30315", "30314", "30316", "30319", "30318", "30321", "30324", "30326", "30325", "30328", "30004", "30327", "30005", "30332", "30331", "30334", "30009", "30337", "30336", "30339", "30342", "30344", "30022", "30349", "30350", "30354", "30358", "30363", "30375", "30384", "30380", "30388", "30398", "30396", "30076", "30075", "30097", "30098"];}
+	if (zipCode == "" || zipCode == null) {zipCode = "Please Specify Zipcode"}
 	if (imprvMax == "" || imprvMax == null) {imprvMax = 10000000;}
-
+``
 	var params = {
 		priceMin: totalPriceMin,
 		priceMax: totalPriceMax,
-		zip: zipCodes,
+		zip: zipCode,
 		imprvMax: imprvMax
 	};
 
-	console.log(`Search request after cleaning: \nmin:${totalPriceMin} max:${totalPriceMax} zip:${zipCodes}`);
-
-
 	var output = [] //this will be our output
-
-    data = fs.readFileSync('./data/Tax_Parcel_TestData.json', 'utf-8');    //Reads in the file
-	const histData = JSON.parse(data.toString());	// converts the file into a local object
-
-	//prints info about the file
-	console.log("Created JSON object:\n  Rows: ", histData.length, "\n  ZIP: ", zipCodes);
-
 	//iterate through each entry
-	for (i=0; i<histData.length; i++) {
-		item = histData[i];
+	for (i=0; i<jsonZip.length; i++) {
+		item = jsonZip[i];
 
 		//check conditions given in the parameters
-		if (zipCodes.includes(item.SITEZIP) &&									//check zipcode
-			item.TOT_APPR >= totalPriceMin && 
+		if (zipCode.includes(item.SITEZIP) &&//check zipcode
+			item.TOT_APPR >= totalPriceMin &&//check price
 			item.TOT_APPR <= totalPriceMax &&
-			item.IMPR_APPR <= imprvMax)	//check price
+			item.IMPR_APPR <= imprvMax)	
 		{
 			output.push(item)	//if yes, add the object to the output
-			console.log("Address: ", item.SITEADDRESS, "   ZIP: ", item.SITEZIP, "   Total Price: ", item.TOT_APPR);
 		}
 	};
 	return([output, params]); //return the data
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express'); // import dependencies
 var bodyParser = require('body-parser');
 const path = require('path');
-const searchFile = require('./searchFile.js')
+const searchFile = require('./searchFile.js');
+// const loadData = require('./uploadData.js');
+const csv = require('csvtojson');
+
 
 const app = express();   //create an instance of express
 const PORT = process.env.PORT || 5000  //Specify the port to lsiten on
@@ -14,41 +17,64 @@ app.set('views', path.join(__dirname, './views'))
 app.use(express.static(__dirname + '/public')); //allows files in /public to be loaded in the Express server
 app.use(express.static(__dirname + '/')); //allows all files to be loaded in the Express server (I think?)
 
+var csvFilePath = './data/Tax_Parcel.csv'
 
-//******* ROUTES ************//
 
-//The landing page of the website.
-app.get('/', (request, response) => {
-	results = null;
-	response.render('pages/index', { results });
+//Load the data
+// const parcelData = loadData.loadData(csvFilePath);
+var jsonZips = {}; //create the data object
+
+//Import data from csv file
+csv().fromFile(csvFilePath).then((jsonObj)=>{
+	jsonObj.forEach(sortIntoZips); 	//sort each address by zipcode into json objects
+
+
+	//******* ROUTES ************//
+	//The landing page of the website.
+	app.get('/', (request, response) => {
+		results = null;
+		response.render('pages/index', { results });
+	});
+
+	app.get('/instructions', (request, response) => {
+		response.render('pages/instructions');
+	});
+
+	app.get('/data', (request, response) => {
+		response.render('pages/data');
+	});
+
+	//Runs when the user navigates to /search. 
+	//Runs a search, displays it as a table on the main page.
+	app.post('/search', urlencodedParser, function(req, res) {
+
+		try {
+			//call search. Get the parameters from the input html, and pass in the specific zipcode object for the query.
+			//returns a 2 item array: [results, parameters]
+			out = searchFile.search(jsonZips[req.body.zip], req.body.priceMin, req.body.priceMax, [String(req.body.zip)], req.body.imprvMax);
+		} catch(error) {
+			//If there is an error with the input (no zipcode input), log it to console
+			console.error(error);
+		} finally {
+			results = out[0]; 
+			params = out[1];
+			res.render('pages/index', { results, params });//Load the HTML page and pass the results
+		}
+	});
+
+	//Server listens for URL addresses here.
+	app.listen(PORT, () => {
+		console.log(`Express server listening on port ${PORT}!`);
+	});
+
 });
 
-app.get('/instructions', (request, response) => {
-	response.render('pages/instructions');
-});
-
-app.get('/data', (request, response) => {
-	response.render('pages/data');
-});
-
-//Runs when the user navigates to <some_url>/search. 
-//Currently runs a search, logs the results to console, and then displays the main page.
-app.post('/search', urlencodedParser, function(req, res) {
-
-	//Calls function search() in searchFile.js
-
-	// searchParameters = `zip:${req.body.zip} price min:${req.body.priceMin} price max:${req.body.priceMax} Improvement Max:${req.body.imprvMax}`;
-	out = searchFile.search(req.body.priceMin, req.body.priceMax, [String(req.body.zip)], req.body.imprvMax);
-	results = out[0]; 
-	params = out[1];
-	console.log(params.priceMin);
-
-	res.render('pages/index', { results, params });//Load the HTML page and pass the results
-});
 
 
-
-//Server listens for URL addresses here.
-app.listen(PORT, () => {
-    console.log(`Express server listening on port ${PORT}!`);
-});
+function sortIntoZips(item, index) {
+    if (jsonZips[item.SITEZIP] == null) {
+        jsonZips[item.SITEZIP] = [item];
+    } else {
+        jsonZips[item.SITEZIP].push(item);
+    }
+};
